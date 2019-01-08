71ae6395573b5e1b7a1205bcb545218df75271d2
yueya <yueyakk@foxmail.com>
2019-01-08 23:13:55 +0800

    访问日志 修改

--- pms-authorization/src/main/java/com/yueya/auth/log/LogInterceptor.java
+++ pms-authorization/src/main/java/com/yueya/auth/log/LogInterceptor.java
@@ -8,13 +8,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class LogInterceptor implements HandlerInterceptor{
     @Autowired
     private LogService logService;
+    private ThreadPoolExecutor threadPool= new ThreadPoolExecutor(5, 100,
+            500L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>(1024), new ThreadPoolExecutor.AbortPolicy());
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         Principal principal=UserInfoUtil.getPrincipal();
-        logService.saveLog(request,handler,ex,principal,null);
+        threadPool.execute(()-> logService.saveLog(request,handler,ex,principal,null));
     }
 }
