7a02cef0f7cb387383ca7591c48b44d532b00335
zhulei <yueyakk@foxmail.com>
2019-03-17 02:21:42 +0800

    区域管理后台，用户管理后台

--- pms-common/src/main/java/com/yueya/common/config/db/PmsDbListener.java
+++ pms-common/src/main/java/com/yueya/common/config/db/PmsDbListener.java
@@ -31,7 +31,7 @@ public class PmsDbListener extends DefaultExecuteListener {
         logger.debug("执行类型："+ctx.type().toString());
         /**
          * 实际上主库数据同步到从库不一定是及时的，如果碰到id是自增长，且插入数据后立刻需要返回插入的id
-         * 的情况，程序可能会读取到错误的数据，所以如果上次是写操作，那么接下来的读操作将立即从主库读取
+         * 的情况，程序可能会读取到错误的数据，所以如果上次是写操作，那么下一次的读操作将立即从主库读取
          */
         //todo 待实际测试
         if(ctx.type().equals(ExecuteType.READ)  && lastIsRead()){
