bc0cd8b45bb99eb04d0ff2d74e37c99f10e460bf
zhulei <yueyakk@foxmail.com>
2019-03-19 11:29:24 +0800

    机构管理接口

--- pms-sysm/src/main/java/com/yueya/system/service/SysOrgService.java
+++ pms-sysm/src/main/java/com/yueya/system/service/SysOrgService.java
@@ -1,6 +1,7 @@
 package com.yueya.system.service;
 
 import com.yueya.common.base.BaseService;
+import com.yueya.system.dao.tables.SysOrganization;
 import com.yueya.system.dao.tables.daos.SysOrganizationDao;
 import com.yueya.system.dao.tables.pojos.SysOrganizationDO;
 import org.jooq.Condition;
@@ -25,6 +26,19 @@ public class SysOrgService extends BaseService<SysOrganizationDO> {
 
     public void update(SysOrganizationDO organizationDO) {
         dao.update(organizationDO);
+    }
+
+    public void delete(String id) {
+        Condition condition = SysOrganization.SYS_ORGANIZATION.ID.eq(Long.valueOf(id))
+                .and(SysOrganization.SYS_ORGANIZATION.PARENT_IDS.like("%,"+id+",%"));
+        SysOrganizationDO organizationDO = new SysOrganizationDO();
+        organizationDO.setDefFlag(DEL_FLAG_DEL);
+        dao.updateByCondition(organizationDO,condition);
+    }
+
+    public List<SysOrganizationDO> findAll() {
+        Condition condition = SysOrganization.SYS_ORGANIZATION.DEF_FLAG.eq(DEL_FLAG_NORMAL);
+        return dao.fetchByCondition(condition);
     }
 
 }
